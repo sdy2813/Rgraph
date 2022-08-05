f6e9f4995da035e0b626aea3e7e4d8fc63efab84
Xum <Xum_sx@163.com>
2022-08-05 14:37:31 +0800

    Add files via upload

---
+++ R_basic/R_setting.R
@@ -0,0 +1,15 @@
+install.packages("pacman")
+pacman::p_install("ggplot2")
+
+pacman::p_install("ggpubr")
+
+pacman::p_install("markdown")
+
+pacman::p_install("rmarkdown")
+
+pacman::p_install("texnity")
+
+
+
+
+
